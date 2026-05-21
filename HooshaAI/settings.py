d0c36fb1c66ff5332dace30665fa23f6390e0f03
Mahdi Abbasi <147281704+Mahdi123408@users.noreply.github.com>
2026-05-21 14:10:43 +0330

    Update settings.py

--- HooshaAI/settings.py
+++ HooshaAI/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,7 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'templates/'
 MEDIA_ROOT = 'templates/'
 # Default primary key field type
